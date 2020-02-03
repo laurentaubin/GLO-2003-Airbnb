7038dcfe758697c4074109c034ea52e8e4e8d7b5
Toma Gagne <toma.gagne.1@ulaval.ca>
2020-02-02 21:57:32 -0500

    commit with test failing

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+  @Test
+  public void given_when_then() {
+    assertTrue(false);
+  }
+}

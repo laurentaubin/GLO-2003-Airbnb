6b784e02a601e998205ba899f0e9a7bc28974bbc
Toma Gagne <toma.gagne.1@ulaval.ca>
2020-02-02 21:36:44 -0500

    main correctly formatted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
-import static  spark.Spark.*;
+import static spark.Spark.*;
 
 public class Main {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
-    }
+  public static void main(String[] args) {
+    get("/hello", (req, res) -> "Hello World");
+  }
 }

ac3892292f6b6651af3ee4e27b89ee32798b71e9
Toma Gagne <toma.gagne.1@ulaval.ca>
2020-02-07 17:57:20 -0500

    deploy on commit test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,17 @@
 import static spark.Spark.*;
 
 public class Main {
+
   public static void main(String[] args) {
-    get("/hello", (req, res) -> "Hello World");
+    port(getHerokuAssignedPort());
+    get("/hello", (req, res) -> "Hello Heroku World");
+  }
+
+  static int getHerokuAssignedPort() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+      return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+    return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
   }
 }

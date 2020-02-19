0f690ca745c042dce78efbfb3c97188b3e7fd46b
Toma Gagne <toma.gagne.1@ulaval.ca>
2020-02-19 09:01:56 -0500

    feature: add StatusResponse class and test

---
+++ src/test/java/StandardResponseTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StandardResponseTest {
+
+  @Test
+  public void createStandardResponse_withNoAttribute_shouldWork() throws Exception {
+    StandardResponse standardResponse = new StandardResponse();
+  }
+}

c91c6324c4486ea12dcff45650ceab21694b2b75
VinceBro <vbreault99@gmail.com>
2020-02-27 11:38:42 -0500

    refactor: cleaned bed package

--- src/main/java/bed/BedDeserializer.java
+++ src/main/java/bed/BedDeserializer.java
@@ -20,36 +20,36 @@ public class BedDeserializer extends JsonDeserializer<Bed> {
 
     bed.setOwnerPublicKey(bedNode.get("ownerPublicKey").textValue());
     bed.setZipCode(bedNode.get("zipCode").textValue());
-    bed.setBedType(Bed.BedType.valueOfLabel(bedNode.get("bedType").textValue()));
+    bed.setBedType(BedType.valueOfLabel(bedNode.get("bedType").textValue()));
     bed.setCleaningFrequency(
-        Bed.CleaningFrequency.valueOfLabel(bedNode.get("cleaningFrequency").textValue()));
+        CleaningFrequency.valueOfLabel(bedNode.get("cleaningFrequency").textValue()));
     bed.setCapacity(bedNode.get("capacity").asInt());
 
-    Bed.BloodType[] bloodTypes = getBedBloodTypes(bedNode.get("bloodTypes"));
+    BloodType[] bloodTypes = getBedBloodTypes(bedNode.get("bloodTypes"));
     bed.setBloodTypes(bloodTypes);
 
-    Bed.BedPackage[] bedPackages = getBedPackages(bedNode.get("packages"));
+    BedPackage[] bedPackages = getBedPackages(bedNode.get("packages"));
     bed.setPackages(bedPackages);
 
     return bed;
   }
 
-  private Bed.BloodType[] getBedBloodTypes(JsonNode bloodTypesNode) {
-    Bed.BloodType[] bloodTypes = new Bed.BloodType[bloodTypesNode.size()];
+  private BloodType[] getBedBloodTypes(JsonNode bloodTypesNode) {
+    BloodType[] bloodTypes = new BloodType[bloodTypesNode.size()];
     for (int i = 0; i < bloodTypes.length; i++) {
-      bloodTypes[i] = Bed.BloodType.valueOfLabel(bloodTypesNode.get(i).textValue());
+      bloodTypes[i] = BloodType.valueOfLabel(bloodTypesNode.get(i).textValue());
     }
     return bloodTypes;
   }
 
-  private Bed.BedPackage[] getBedPackages(JsonNode packagesNode) {
-    Bed.BedPackage[] bedPackages = new Bed.BedPackage[packagesNode.size()];
+  private BedPackage[] getBedPackages(JsonNode packagesNode) {
+    BedPackage[] bedPackages = new BedPackage[packagesNode.size()];
     for (int i = 0; i < bedPackages.length; i++) {
 
-      Bed.BedPackage.Name name =
-          Bed.BedPackage.Name.valueOfLabel(packagesNode.get(i).get("name").textValue());
+      BedPackage.Name name =
+          BedPackage.Name.valueOfLabel(packagesNode.get(i).get("name").textValue());
       double pricePerNight = packagesNode.get(i).get("pricePerNight").asDouble();
-      Bed.BedPackage bedPackage = new Bed.BedPackage(name, pricePerNight);
+      BedPackage bedPackage = new BedPackage(name, pricePerNight);
       bedPackages[i] = bedPackage;
     }
     return bedPackages;

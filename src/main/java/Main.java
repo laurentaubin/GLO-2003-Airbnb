172736956a9693d996cf227f54c2c8348adbb23d
jcdrouin <jcdrouin21@icloud.com>
2020-02-27 13:11:41 -0500

    the code format validation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
-import bed.Bed;
-
 import static spark.Spark.*;
 
 public class Main {
-
   public static void main(String[] args) {
     port(Server.getHerokuAssignedPort());
     Router.setUpRoutes();
     CORS.enable("*", Server.CORS_HEADERS, Server.CORS_METHODS);
-
   }
 }

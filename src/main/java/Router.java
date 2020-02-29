81be737318e67cdf21ae436808a26bbd6cc8f8b3
Toma Gagne <toma.gagne.1@ulaval.ca>
2020-02-29 14:36:42 -0500

    fix router

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,11 +1,9 @@
 import static spark.Spark.path;
 
 import bed.BedResource;
-import testclass.MovieResource;
 
 public class Router {
   public static void setUpRoutes() {
     path(BedResource.ROOT_PATH, new BedResource());
-    path(MovieResource.ROOT_PATH, new MovieResource());
   }
 }

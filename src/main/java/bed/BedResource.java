325472c6e18c18b866a754b3a94582df963507b5
Benjamin Girard <benjamin.girard.protic@gmail.com>
2020-02-28 22:52:18 -0500

    Fixed route

--- src/main/java/bed/BedResource.java
+++ src/main/java/bed/BedResource.java
@@ -10,26 +10,21 @@ import spark.Response;
 import spark.RouteGroup;
 
 public class BedResource implements RouteGroup {
-  public static final String ROOT_PATH = "/beds/";
+  public static final String ROOT_PATH = "/";
   private BedService bedService = new BedService();
 
   @Override
   public void addRoutes() {
-    get("/", this::getBeds, new ObjectMapper()::writeValueAsString);
+    get("/beds", this::getBeds, new ObjectMapper()::writeValueAsString);
   }
 
   public Object getBeds(Request request, Response response) {
 
     String packageNames = request.queryParamOrDefault("package", "empty");
-
     String bedTypes = request.queryParamOrDefault("bedType", "empty");
-
     String cleaningFrequencies = request.queryParamOrDefault("cleaningFreq", "empty");
-
     String bloodTypes = request.queryParamOrDefault("bloodTypes", "empty");
-
     String minCapacity = request.queryParamOrDefault("minCapacity", "0");
-
     Query query = new Query(packageNames, bedTypes, cleaningFrequencies, bloodTypes, minCapacity);
     ArrayList<Bed> beds = this.bedService.Get(query);
 

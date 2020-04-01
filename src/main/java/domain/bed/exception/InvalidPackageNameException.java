9b7309765ce438fa62b3d31ad62a72806ed7352d
Toma Gagne <46379340+TomaGagne@users.noreply.github.com>
2020-04-01 00:41:08 -0400

    Fix typo in InvalidPackageNameException (#154)
    Co-authored-by: laurentaubin <44675437+laurentaubin@users.noreply.github.com>
    Co-authored-by: Vincent Lambert <46732431+vincentlambert8@users.noreply.github.com>

--- src/main/java/domain/bed/exception/InvalidPackageNameException.java
+++ src/main/java/domain/bed/exception/InvalidPackageNameException.java
@@ -3,7 +3,7 @@ package domain.bed.exception;
 public class InvalidPackageNameException extends AirbnbException {
   public InvalidPackageNameException() {
     super(
-        "INVALID_PACKAGES",
+        "INVALID_PACKAGE",
         "packages should be an array of package name with " + "its corresponding price per night");
   }
 }
